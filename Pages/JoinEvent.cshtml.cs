43b4ba2bd0b0bd75138c1eaf26c5aa66fc55e44f
Jens <jens.gost@gmail.com>
2021-05-08 14:55:20 +0200

    Created JoinEvent-page

---
+++ Pages/JoinEvent.cshtml.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebbApplicationASPNetCoreVersion2.Data;
+using WebbApplicationASPNetCoreVersion2.Models;
+
+namespace WebbApplicationASPNetCoreVersion2.Pages
+{
+    public class JoinEventModel : PageModel
+    {
+        private readonly WebbApplicationASPNetCoreVersion2.Data.EventDbContext _context;
+
+        public JoinEventModel(WebbApplicationASPNetCoreVersion2.Data.EventDbContext context)
+        {
+            _context = context;
+        }
+
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events.Include(e => e.Attendees).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+
+            var attendee = await _context.Attendees.FirstOrDefaultAsync();
+
+            if (!Event.Attendees.Contains(attendee))
+            {
+                Event.Attendees.Add(attendee);
+                await _context.SaveChangesAsync();
+            }
+
+            return Page();
+        }
+    }
+}
