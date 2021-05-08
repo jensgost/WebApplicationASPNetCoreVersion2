f1220c2aee4f720558a34ee89234f4e58aa1288a
Jens <jens.gost@gmail.com>
2021-05-08 14:42:53 +0200

    Events-page created

---
+++ Pages/Events.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EventApp.Data;
+using EventApp.Models;
+
+namespace WebbApplicationASPNetCoreVersion2.Pages
+{
+    public class EventsModel : PageModel
+    {
+        private readonly EventContext _context;
+
+        public EventsModel(EventContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Event { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Event = await _context.Events.Include(e => e.Organizer).ToListAsync();
+        }
+    }
+}
